aaff7bdb7d49ae0b7827534c33202a6316de7df0
alecthw <alecthw@163.com>
2020-08-12 10:27:11 +0800

    update

--- user/x64-1907/custom.sh
+++ user/x64-1907/custom.sh
@@ -2,9 +2,6 @@
 
 echo "Test custom.sh"
 
-version=`date "+%m.%d"`
-echo $1
-
 sed -i "/^#uci set network.lan.ipaddr/cuci set network.lan.ipaddr='192.168.11.1'"    package/default-settings/files/zzz-default-settings
 sed -i "/^#uci set network.lan.netmask/cuci set network.lan.netmask='255.255.255.0'" package/default-settings/files/zzz-default-settings
 sed -i "/^#uci commit network/cuci commit network"                                   package/default-settings/files/zzz-default-settings
