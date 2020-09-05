bfbc35f4107c57ae70edd0e20e2d2e2be6451de8
alecthw <alecthw@163.com>
2020-09-05 23:38:59 +0800

    update

--- user/lienol-master-x64/custom.sh
+++ user/lienol-master-x64/custom.sh
@@ -2,6 +2,10 @@
 
 echo "Test custom.sh"
 
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/node-request.git           package/node-request
+git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
+
 sed -i "/^#uci set network.lan.ipaddr/cuci set network.lan.ipaddr='192.168.11.1'"    package/default-settings/files/zzz-default-settings
 sed -i "/^#uci set network.lan.netmask/cuci set network.lan.netmask='255.255.255.0'" package/default-settings/files/zzz-default-settings
 sed -i "/^#uci commit network/cuci commit network"                                   package/default-settings/files/zzz-default-settings
