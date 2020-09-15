6a9e68e2501bca7e363f2bb06cbd012440ca0199
alecthw <alecthw@163.com>
2020-09-15 16:14:33 +0800

    update

--- user/lede-x64/custom.sh
+++ user/lede-x64/custom.sh
@@ -7,6 +7,6 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 sed -i "/uci commit fstab/a\uci commit network"                          package/lean/default-settings/files/zzz-default-settings
 sed -i "/uci commit fstab/a\uci set network.lan.netmask='255.255.255.0'" package/lean/default-settings/files/zzz-default-settings
 sed -i "/uci commit fstab/a\uci set network.lan.ipaddr='192.168.11.1'"   package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit fstab/a\ "                                           package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit fstab/G"                                             package/lean/default-settings/files/zzz-default-settings
 
 cat package/lean/default-settings/files/zzz-default-settings
