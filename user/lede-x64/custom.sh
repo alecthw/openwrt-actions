69ccee5c72183cd554a353ae48279d125ecdf3be
alecthw <alecthw@163.com>
2020-09-15 15:37:32 +0800

    fix error

--- user/lede-x64/custom.sh
+++ user/lede-x64/custom.sh
@@ -9,4 +9,4 @@ sed -i "/uci commit fstab/a\uci set network.lan.netmask='255.255.255.0'" package
 sed -i "/uci commit fstab/a\uci set network.lan.ipaddr='192.168.11.1'"   package/lean/default-settings/files/zzz-default-settings
 sed -i "/uci commit fstab/a\ "                                           package/lean/default-settings/files/zzz-default-settings
 
-cat package/default-settings/files/zzz-default-settings
+cat package/lean/default-settings/files/zzz-default-settings
