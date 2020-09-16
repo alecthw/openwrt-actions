bec9934e0122e9a5030bc58c5147158878b4d6b3
alecthw <alecthw@163.com>
2020-09-16 14:32:19 +0800

    set default luci theme for lede-x64

--- user/lede-x64/custom.sh
+++ user/lede-x64/custom.sh
@@ -4,9 +4,11 @@ echo "Test custom.sh"
 
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
-sed -i "/uci commit fstab/a\uci commit network"                          package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit fstab/a\uci set network.lan.netmask='255.255.255.0'" package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit fstab/a\uci set network.lan.ipaddr='192.168.11.1'"   package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit fstab/G"                                             package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/material"  package/lean/default-settings/files/zzz-default-settings
+
+sed -i "/uci commit fstab/a\uci commit network"                                   package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit fstab/a\uci set network.lan.netmask='255.255.255.0'"          package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit fstab/a\uci set network.lan.ipaddr='192.168.11.1'"            package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit fstab/G"                                                      package/lean/default-settings/files/zzz-default-settings
 
 cat package/lean/default-settings/files/zzz-default-settings
