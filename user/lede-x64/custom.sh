5d770d67c540488f64690cae3302acafd2d8c106
alecthw <alecthw@163.com>
2020-09-16 16:00:25 +0800

    update

--- user/lede-x64/custom.sh
+++ user/lede-x64/custom.sh
@@ -2,10 +2,24 @@
 
 echo "Test custom.sh"
 
+# add custom packages
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
+# clean default config of https-dns-proxy
+cat > feeds/packages/net/https-dns-proxy/files/https-dns-proxy.config << EOF
+config main 'config'
+	option update_dnsmasq_config ''
+EOF
+
+# add luci https
+sed -i "/	list listen_http	[::]:80/a\	list listen_https	[::]:443"         package/network/services/uhttpd/files/uhttpd.config
+sed -i "/	list listen_http	[::]:80/a\	list listen_https	0.0.0.0:443"      package/network/services/uhttpd/files/uhttpd.config
+sed -i "/	list listen_http	[::]:80/G"                                        package/network/services/uhttpd/files/uhttpd.config
+
+# set default theme
 sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/material"  package/lean/default-settings/files/zzz-default-settings
 
+# set lan ip
 sed -i "/uci commit fstab/a\uci commit network"                                   package/lean/default-settings/files/zzz-default-settings
 sed -i "/uci commit fstab/a\uci set network.lan.netmask='255.255.255.0'"          package/lean/default-settings/files/zzz-default-settings
 sed -i "/uci commit fstab/a\uci set network.lan.ipaddr='192.168.11.1'"            package/lean/default-settings/files/zzz-default-settings
--- user/lienol-1907-x64/custom.sh
+++ user/lienol-1907-x64/custom.sh
@@ -2,6 +2,7 @@
 
 echo "Test custom.sh"
 
+# set lan ip
 sed -i "/^#uci set network.lan.ipaddr/cuci set network.lan.ipaddr='192.168.11.1'"    package/default-settings/files/zzz-default-settings
 sed -i "/^#uci set network.lan.netmask/cuci set network.lan.netmask='255.255.255.0'" package/default-settings/files/zzz-default-settings
 sed -i "/^#uci commit network/cuci commit network"                                   package/default-settings/files/zzz-default-settings
