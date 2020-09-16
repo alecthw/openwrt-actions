cdb51514715094d2ccef6624d6261d55d134262c
alecthw <alecthw@163.com>
2020-09-16 16:28:49 +0800

    update

--- user/lede-wrt1900acs/custom.sh
+++ user/lede-wrt1900acs/custom.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "Test custom.sh"
+
+# set default theme
+sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/material"  package/lean/default-settings/files/zzz-default-settings
