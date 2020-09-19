f044baedfd9fb2a5c657b3b7420af026b2a5aaa9
alecthw <alecthw@163.com>
2020-09-19 16:21:01 +0800

    update

--- user/lienol-master-x64/custom.sh
+++ user/lienol-master-x64/custom.sh
@@ -4,7 +4,6 @@ echo "Test custom.sh"
 
 # add custom packages
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/node-request.git           package/node-request
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
 
 # set lan ip
