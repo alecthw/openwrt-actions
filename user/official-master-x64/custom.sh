8dfc25ab50510ee1ee2dd635321e9e497595a4f3
idn <idn@idn.com>
2020-09-23 17:28:59 +0800

    add

--- user/official-master-x64/custom.sh
+++ user/official-master-x64/custom.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "Test custom.sh"
+
+# copy default config
+cp -f ../user/official-master-x64/defconfig/etc/config/adbyby           package/lean/luci-app-adbyby-plus/root/etc/config/adbyby
+
+cp -f ../user/official-master-x64/defconfig/etc/config/openclash        package/feeds/openclash/luci-app-openclash/root/etc/config/openclash
+
+cp -f ../user/official-master-x64/defconfig/etc/config/passwall         package/feeds/diy1/luci-app-passwall/root/etc/config/passwall
+cp -f ../user/official-master-x64/defconfig/usr/share/passwall/rules/*  package/feeds/diy1/luci-app-passwall/root/usr/share/passwall/rules/
+
+mkdir -p package/feeds/luci/luci-app-smartdns/root/etc/config
+mkdir -p package/feeds/luci/luci-app-smartdns/root/etc/smartdns
+cp -f ../user/official-master-x64/defconfig/etc/config/smartdns         package/feeds/luci/luci-app-smartdns/root/etc/config/smartdns
+cp -f ../user/official-master-x64/defconfig/etc/smartdns/custom.conf    package/feeds/luci/luci-app-smartdns/root/etc/smartdns/custom.conf 
