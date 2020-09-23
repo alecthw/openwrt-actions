fcd63092e719c89581b1e585a01b182cbf548748
alecthw <alecthw@163.com>
2020-09-23 11:01:46 +0800

    update

--- user/lienol-master-x64/custom.sh
+++ user/lienol-master-x64/custom.sh
@@ -6,22 +6,6 @@ echo "Test custom.sh"
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
 
-# add luci-app-serverchan acl
-mkdir -p package/luci-app-serverchan/root/usr/share/rpcd/acl.d
-cat > package/luci-app-serverchan/root/usr/share/rpcd/acl.d/luci-app-serverchan.json << EOF
-{
-	"luci-app-serverchan": {
-		"description": "Grant UCI access for luci-app-serverchan",
-		"read": {
-			"uci": [ "serverchan" ]
-		},
-		"write": {
-			"uci": [ "serverchan" ]
-		}
-	}
-}
-EOF
-
 # set lan ip
 sed -i "/^#uci set network.lan.ipaddr/cuci set network.lan.ipaddr='192.168.11.1'"    package/default-settings/files/zzz-default-settings
 sed -i "/^#uci set network.lan.netmask/cuci set network.lan.netmask='255.255.255.0'" package/default-settings/files/zzz-default-settings
