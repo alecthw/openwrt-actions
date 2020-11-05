0fc2a971e34afc215564175997c55f6f5d9cab6b
alecthw <alecthw@163.com>
2020-11-05 10:38:15 +0800

    disable some config

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -3,5 +3,5 @@
 echo "Apply custom.sh"
 
 # add custom firewall zone
-sed -i "/'lan'/a\	list   network		'n2n0'"    package/network/config/firewall/files/firewall.config
-sed -i "/'wan6'/a\	list   network		'iptv'"    package/network/config/firewall/files/firewall.config
+#sed -i "/'lan'/a\	list   network		'n2n0'"    package/network/config/firewall/files/firewall.config
+#sed -i "/'wan6'/a\	list   network		'iptv'"    package/network/config/firewall/files/firewall.config
