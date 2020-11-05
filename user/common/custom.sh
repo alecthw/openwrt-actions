cdea45f028de1b6d667ec91b0b9442ffa07a548d
idn <idn@idn.com>
2020-11-05 10:02:53 +0800

    update

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -2,3 +2,6 @@
 
 echo "Apply custom.sh"
 
+# add custom firewall zone
+sed -i "/'lan'/a\	list   network		'n2n0'"    package/network/config/firewall/files/firewall.config
+sed -i "/'wan6'/a\	list   network		'iptv'"    package/network/config/firewall/files/firewall.config
