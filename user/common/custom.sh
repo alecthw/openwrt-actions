82b542f4c39d9bd75d42078c287c8983b17acfae
Alec Tian <alecthw@163.com>
2020-11-10 22:08:26 +0800

    update

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -2,6 +2,8 @@
 
 echo "Apply custom.sh"
 
+git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06    package/luci-theme-argon-jerrykuku
+
 # add custom firewall zone
 #sed -i "/'lan'/a\	list   network		'n2n0'"    package/network/config/firewall/files/firewall.config
 #sed -i "/'wan6'/a\	list   network		'iptv'"    package/network/config/firewall/files/firewall.config
