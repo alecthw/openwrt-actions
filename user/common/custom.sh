5fdd356fee8dcaefe6f5e33f0d37b6cee41e23d8
idn <idn@idn.com>
2021-10-09 16:43:20 +0800

    subscribe.lua

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -10,6 +10,29 @@ source=${array[0]}
 echo "source=${source}"
 
 do_common() {
+    # modify passwall subscribe.lua
+    subscribe_script="package/feeds/passwall/luci-app-passwall/root/usr/share/passwall/subscribe.lua"
+    if [ -f "$subscribe_script" ]; then
+        sed -i '/第一优先级/i\		-- 自定义优先级 订阅 + 类型 + 备注' $subscribe_script
+        sed -i '/第一优先级/i\		if not server then' $subscribe_script
+        sed -i '/第一优先级/i\			for index, node in pairs(nodes) do' $subscribe_script
+        sed -i '/第一优先级/i\				if config.currentNode.add_from and config.currentNode.type and config.currentNode.remarks then' $subscribe_script
+        sed -i '/第一优先级/i\					if node.add_from and node.type and node.remarks then' $subscribe_script
+        sed -i '/第一优先级/i\						if node.add_from == config.currentNode.add_from and node.type == config.currentNode.type and node.remarks == config.currentNode.remarks then' $subscribe_script
+        sed -i '/第一优先级/i\							if config.log == nil or config.log == true then' $subscribe_script
+        sed -i "/第一优先级/i\								log('更新【' .. config.remarks .. '】自定义匹配节点：' .. node.remarks)" $subscribe_script
+        sed -i '/第一优先级/i\							end' $subscribe_script
+        sed -i '/第一优先级/i\							server = node[".name"]' $subscribe_script
+        sed -i '/第一优先级/i\							break' $subscribe_script
+        sed -i '/第一优先级/i\						end' $subscribe_script
+        sed -i '/第一优先级/i\					end' $subscribe_script
+        sed -i '/第一优先级/i\				end' $subscribe_script
+        sed -i '/第一优先级/i\			end' $subscribe_script
+        sed -i '/第一优先级/i\		end' $subscribe_script
+
+        sed -i 's/log = false/log = true/g' $subscribe_script
+    fi
+
     # copy default config
     if [ -d "../defconfig" ]; then
         cp -f ../defconfig/zzz-extra-settings package/base-files/files/etc/uci-defaults/zzzz-extra-settings
