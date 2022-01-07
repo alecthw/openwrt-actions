da31eb853714e796afe72fc264a24e7831c0eb3a
alecthw <alecthw@163.com>
2022-01-07 16:48:54 +0800

    fix openclash complie error

--- user/lienol-1907-x64/patch.sh
+++ user/lienol-1907-x64/patch.sh
@@ -6,3 +6,4 @@ echo "Execute custom patch.sh"
 
 # add custom packages
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+svn co https://github.com/Lienol/openwrt/trunk/package/libs/libcap package/libs/libcap
