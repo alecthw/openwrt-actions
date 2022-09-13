1b0222fcc3ed6ff244070b90271127c6bf0ac4d5
alecthw <alecthw@163.com>
2022-09-13 17:10:40 +0800

    compile error

--- user/lede-x64/patch.sh
+++ user/lede-x64/patch.sh
@@ -7,4 +7,5 @@ echo "Execute custom patch.sh"
 # add custom packages
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
