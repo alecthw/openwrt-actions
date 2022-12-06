d99cc2d0d9057afe7a217bcdcbd190223062c5ee
alecthw <alecthw@163.com>
2022-12-06 14:38:35 +0800

    update

--- user/lienol-main-x64/patch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Execute before update feeds
-
-echo "Execute custom patch.sh"
-
-# add custom packages
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
