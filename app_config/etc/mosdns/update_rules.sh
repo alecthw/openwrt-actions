d508ddbaa288e5a677e594dab0b6526230813998
alecthw <alecthw@163.com>
2024-02-01 14:36:08 +0800

    fix mosdns rule update problem when gh-proxy error

--- app_config/etc/mosdns/update_rules.sh
+++ app_config/etc/mosdns/update_rules.sh
@@ -12,25 +12,25 @@ github_prefix=${github_prefix1}
 tmp_dir=$(mktemp -d) || exit 1
 
 # Dwonload reject-list.txt
-curl -skL --retry 3 --connect-timeout 3 -o ${tmp_dir}/reject-list.txt ${github_prefix}https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt
+http_code=$(curl -skL --retry 3 --connect-timeout 3 -w %{http_code} -o ${tmp_dir}/reject-list.txt ${github_prefix}https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt)
 
-if [ $? == 0 ]; then
+if [ $? == 0 -a $http_code == 200 ]; then
     echo "Dwonload reject-list success"
     mv -f ${tmp_dir}/reject-list.txt /etc/mosdns/rule/reject-list.txt
 fi
 
 # Dwonload whitelist.list
-curl -skL --retry 3 --connect-timeout 3 -o ${tmp_dir}/cn-white.txt ${github_prefix}https://raw.githubusercontent.com/alecthw/chnlist/release/mosdns/whitelist.list
+http_code=$(curl -skL --retry 3 --connect-timeout 3 -w %{http_code} -o ${tmp_dir}/cn-white.txt ${github_prefix}https://raw.githubusercontent.com/alecthw/chnlist/release/mosdns/whitelist.list)
 
-if [ $? == 0 ]; then
+if [ $? == 0 -a $http_code == 200 ]; then
     echo "Dwonload cn-white success"
     mv -f ${tmp_dir}/cn-white.txt /etc/mosdns/rule/cn-white.txt
 fi
 
 # Dwonload Country.mmdb
-curl -skL --retry 3 --connect-timeout 3 -o ${tmp_dir}/Country.mmdb ${github_prefix}https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb
+http_code=$(curl -skL --retry 3 --connect-timeout 3 -w %{http_code} -o ${tmp_dir}/Country.mmdb ${github_prefix}https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb)
 
-if [ $? == 0 ]; then
+if [ $? == 0 -a $http_code == 200 ]; then
     echo "Dwonload mmdb success"
     mv -f ${tmp_dir}/Country.mmdb /etc/mosdns/rule/Country.mmdb
 fi
