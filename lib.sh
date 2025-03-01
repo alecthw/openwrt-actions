143486aac5a157a88f9420d8002e2e6cac0890bc
alecthw <alecthw@163.com>
2025-03-01 18:34:01 +0800

    optimize download logic

--- lib.sh
+++ lib.sh
@@ -28,7 +28,7 @@ dl_svn() {
         rm -rf $tmp_dir/tmp/.svn
 
         mkdir -p $2
-        mv -f $tmp_dir/tmp/* $2/
+        cp -af $tmp_dir/tmp/* $2/
     else
         echo "Error: Svn download fail: $1"
     fi
@@ -53,7 +53,7 @@ dl_git() {
         rm -rf $tmp_dir/tmp/.git
 
         mkdir -p $2
-        mv -f $tmp_dir/tmp/* $2/
+        cp -af $tmp_dir/tmp/* $2/
     else
         echo "Error: Git download fail: $1"
     fi
@@ -66,35 +66,39 @@ dl_git() {
 # $3: sub dir
 # $4: branch
 dl_git_sub() {
-    tmp_dir=$(mktemp -d)
+    mkdir -p /tmp/opbuild
 
-    if [ -z $4 ]; then
-        git clone -q --depth=1 --single-branch $1 $tmp_dir/tmp
+    url_brahch_hash=$(echo "$1/tree/$4" | md5sum | cut -c 1-6)
+
+    tmp_dir=/tmp/opbuild/$url_brahch_hash
+
+    if [ ! -d "$tmp_dir" ];then
+        if [ -z $4 ]; then
+            git clone -q --depth=1 --single-branch $1 $tmp_dir
+        else
+            git clone -q --depth=1 --single-branch -b $4 $1 $tmp_dir
+        fi
     else
-        git clone -q --depth=1 --single-branch -b $4 $1 $tmp_dir/tmp
+        echo "Warn: Git-sub download aleady exist: $1/tree/$4"
     fi
 
     if [ $? == 0 ]; then
         echo "Info: Git-sub download success: $1/tree/$4/$3"
-        rm -rf $tmp_dir/tmp/.git
-
-        src_dir=$tmp_dir/tmp
+        src_dir=$tmp_dir
 
         if [ -n $3 ]; then
-            src_dir=$tmp_dir/tmp/$3
+            src_dir=$tmp_dir/$3
         fi
 
         if [ -e $src_dir ]; then
             mkdir -p $2
-            mv -f $src_dir/* $2/
+            cp -af $src_dir/* $2/
         else
             echo "Error: Git-sub copy fail not exist: $src_dir"
         fi
     else
         echo "Error: Git-sub download fail: $1/tree/$4/$3"
     fi
-
-    rm -rf $tmp_dir
 }
 
 # $1: url
@@ -109,7 +113,7 @@ dl_curl() {
         rm -rf $tmp_dir/.git
 
         mkdir -p ${2%/*}
-        mv -f $tmp_dir/tmp $2
+        cp -af $tmp_dir/tmp $2
     else
         echo "Error: Download fail: $1"
     fi
