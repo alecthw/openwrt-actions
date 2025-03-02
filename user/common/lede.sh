548aa228f7340e840008be9e4690aacb4e499c36
alecthw <alecthw@163.com>
2025-03-02 21:51:12 +0800

    replace fstools, fix read-only when reboot

--- user/common/lede.sh
+++ user/common/lede.sh
@@ -14,6 +14,11 @@ do_lede_common() {
     sed -i "/shadow/d" package/lean/default-settings/files/zzz-default-settings
     # delete 53 redirect
     sed -i '/REDIRECT --to-ports 53/d' package/lean/default-settings/files/zzz-default-settings
+
+
+    # replace fstools, fix read-only when reboot
+    rm -rf package/system/fstools
+    dl_git_sub https://github.com/immortalwrt/immortalwrt package/system/fstools package/system/fstools openwrt-24.10
 }
 
 # excute
